df91e6a289cebb73c3b892c18e737925db494692
Arkueid <thetardis@qq.com>
2024-06-18 22:39:18 +0800

    通过模型版本变量控制配置文件和模型文件

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from . import settings, define
 
 if settings.LIVE2D_VERSION == define.Live2DVersion.V3:
-    if settings.APP_MODE == define.AppMode.DEBUG:
-        import live2d.v3.debug as live2d
-    elif settings.APP_MODE == define.AppMode.RELEASE:
-        import live2d.v3 as live2d
-    else:
-        raise Exception("Unknown app mode: %s", settings.APP_MODE)
+    import live2d.v3 as live2d
 elif settings.LIVE2D_VERSION == define.Live2DVersion.V2:
     import live2d.v2 as live2d
 else:
     raise Exception("Unknown live2d version: %s", settings.LIVE2D_VERSION)
+
+
+if settings.APP_MODE == define.AppMode.DEBUG:
+    live2d.setLogEnable(True)
+elif settings.APP_MODE == define.AppMode.RELEASE:
+    live2d.setLogEnable(False)
+else:
+    raise Exception("Unknown app mode: %s", settings.APP_MODE)
 
 __all__ = ['live2d', 'settings', 'define']
--- app/settings.py
+++ app/settings.py
@@ -2,13 +2,13 @@ from app.define import AppMode, Live2DVersion
 
 APP_MODE = AppMode.DEBUG
 
-LIVE2D_VERSION = Live2DVersion.V2
-
-CONFIG_PATH = "./config.json"
+LIVE2D_VERSION = Live2DVersion.V3
 
 if LIVE2D_VERSION == Live2DVersion.V3:
     MODEL_JSON_SUFFIX = ".model3.json"
+    CONFIG_PATH = "./config.v3.json"
 elif LIVE2D_VERSION == Live2DVersion.V2:
-    MODEL_JSON_SUFFIX = ".model3.json"
+    MODEL_JSON_SUFFIX = ".model.json"
+    CONFIG_PATH = "./config.v2.json"
 else:
     raise Exception("Unknown live2d version: %s", LIVE2D_VERSION)
